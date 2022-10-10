a8315a82619ec0969d18a71ac61e3fbc4eeba79b
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2022-10-10 13:18:32 +0300

    add portal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,12 +1,14 @@
-import {FaRegWindowClose} from 'react-icons/fa'
+import {createPortal} from 'react-dom';
+import {FaRegWindowClose} from 'react-icons/fa';
+
+const modalRoot = document.querySelector('#modal-root');
 const  Modal = ({largeImage, closeModalWindow, handleKeyDown}) => { 
-   return (
+   return (createPortal(
     <div className='Overlay' onClick={closeModalWindow} onKeyDown={handleKeyDown} tabIndex={0}>
       <div className='Modal' onClick={(e)=>e.stopPropagation()} style={{backgroundImage:`url(${largeImage.src})`}}> 
          <button className="button" type="button" onClick={(e)=>closeModalWindow(e)}><FaRegWindowClose style={{width:30, height: 30, fill: "blue"}}/></button>
-         {/* <img className= 'imgModal' src = {largeImage.src} alt={largeImage.alt} /> */}
       </div>
-   </div>
+   </div>, modalRoot)
  )       
 }
 export default Modal
